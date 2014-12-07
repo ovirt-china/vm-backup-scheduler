31b62add90aab55babb2263ec882078ae5a33c9c
walteryang <walteryang47@users.noreply.github.com>
2014-12-08 01:26:37 +0800

    Move createSnapshot and querySnapshot to TimerSDKTask
    They are going to be used by other implementation for example:
    ExecuteExport
    
    Signed-off-by: walteryang <walteryang47@users.noreply.github.com>

--- src/main/java/org/ovirtChina/enginePlugin/vmBackupScheduler/bll/ExecuteSnapshot.java
+++ src/main/java/org/ovirtChina/enginePlugin/vmBackupScheduler/bll/ExecuteSnapshot.java
@@ -1,22 +1,22 @@
 package org.ovirtChina.enginePlugin.vmBackupScheduler.bll;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import org.apache.http.client.ClientProtocolException;
 import org.ovirt.engine.sdk.decorators.VM;
-import org.ovirt.engine.sdk.entities.Snapshot;
 import org.ovirt.engine.sdk.exceptions.ServerException;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.common.Task;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.common.TaskStatus;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.common.TaskType;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.dao.DbFacade;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.utils.OVirtEngineSDKUtils;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExecuteSnapshot extends TimerSDKTask {
-    private static Logger log = LoggerFactory.getLogger(ExecuteSnapshot.class);
+
+    public ExecuteSnapshot() {
+        log = LoggerFactory.getLogger(ExecuteSnapshot.class);
+    }
 
     protected void peformAction() throws ClientProtocolException, ServerException, IOException {
         Task taskToExec = DbFacade.getInstance().getTaskDAO().getOldestTaskTypeWithStatus(TaskType.CreateSnapshot, TaskStatus.EXECUTING);
@@ -30,13 +30,7 @@ public class ExecuteSnapshot extends TimerSDKTask {
         api = OVirtEngineSDKUtils.getApi();
         if (api != null) {
             if (taskToExec.getTaskStatus() == TaskStatus.WAITING.getValue()) {
-                Snapshot snap = new Snapshot();
-                VM vm = api.getVMs().get(taskToExec.getVmID());
-                snap.setVm(vm);
-                snap.setDescription("autoSnap");
-                String snapshotId = vm.getSnapshots().add(snap).getId();
-                taskToExec.setBackupName(snapshotId);
-                setTaskStatus(taskToExec, TaskStatus.EXECUTING);
+                VM vm = createSnapshot(taskToExec);
                 try {
                     querySnapshot(taskToExec, vm);
                 } catch (InterruptedException e) {
@@ -45,13 +39,6 @@ public class ExecuteSnapshot extends TimerSDKTask {
                 }
                 setTaskStatus(taskToExec, TaskStatus.FINISHED);
             }
-        }
-    }
-
-    private void querySnapshot(Task taskToExec, VM vm) throws ClientProtocolException, ServerException, IOException, InterruptedException {
-        while(!api.getVMs().get(UUID.fromString(vm.getId())).getSnapshots().getById(taskToExec.getBackupName()).getSnapshotStatus().equals("ok")) {
-            log.info("vm: " + vm.getName() + " is snapshoting, waiting for next query...");
-            Thread.sleep(5000);
         }
     }
 }
--- src/main/java/org/ovirtChina/enginePlugin/vmBackupScheduler/bll/TimerSDKTask.java
+++ src/main/java/org/ovirtChina/enginePlugin/vmBackupScheduler/bll/TimerSDKTask.java
@@ -3,17 +3,22 @@ package org.ovirtChina.enginePlugin.vmBackupScheduler.bll;
 import java.io.IOException;
 import java.util.Date;
 import java.util.TimerTask;
+import java.util.UUID;
 
 import org.apache.http.client.ClientProtocolException;
 import org.ovirt.engine.sdk.Api;
 import org.ovirt.engine.sdk.decorators.StorageDomain;
+import org.ovirt.engine.sdk.decorators.VM;
+import org.ovirt.engine.sdk.entities.Snapshot;
 import org.ovirt.engine.sdk.exceptions.ServerException;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.common.Task;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.common.TaskStatus;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.dao.DbFacade;
+import org.slf4j.Logger;
 
 public abstract class TimerSDKTask extends TimerTask {
 
+    protected static Logger log;
     protected Api api = null;
 
     @Override
@@ -46,6 +51,24 @@ public abstract class TimerSDKTask extends TimerTask {
             }
         }
         return null;
+    }
+
+    protected void querySnapshot(Task taskToExec, VM vm) throws ClientProtocolException, ServerException, IOException, InterruptedException {
+        while(!api.getVMs().get(UUID.fromString(vm.getId())).getSnapshots().getById(taskToExec.getBackupName()).getSnapshotStatus().equals("ok")) {
+            log.info("vm: " + vm.getName() + " is snapshoting, waiting for next query...");
+            Thread.sleep(5000);
+        }
+    }
+
+    protected VM createSnapshot(Task taskToExec) throws ClientProtocolException, ServerException, IOException {
+        Snapshot snap = new Snapshot();
+        VM vm = api.getVMs().get(taskToExec.getVmID());
+        snap.setVm(vm);
+        snap.setDescription("autoSnap");
+        String snapshotId = vm.getSnapshots().add(snap).getId();
+        taskToExec.setBackupName(snapshotId);
+        setTaskStatus(taskToExec, TaskStatus.EXECUTING);
+        return vm;
     }
 
     protected abstract void peformAction() throws Exception;
