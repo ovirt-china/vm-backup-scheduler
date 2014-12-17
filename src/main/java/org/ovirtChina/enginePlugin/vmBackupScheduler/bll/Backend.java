5ef6d95f790b38b252e9779ad661257f2c9b835f
walteryang <walteryang47@gmail.com>
2014-12-17 16:29:14 +0800

    Always retry getting api connection while service starts up
    Signed-off-by: walteryang <walteryang47@gmail.com>

--- src/main/java/org/ovirtChina/enginePlugin/vmBackupScheduler/bll/Backend.java
+++ src/main/java/org/ovirtChina/enginePlugin/vmBackupScheduler/bll/Backend.java
@@ -1,5 +1,7 @@
 package org.ovirtChina.enginePlugin.vmBackupScheduler.bll;
 
+import java.io.IOException;
+import java.net.NoRouteToHostException;
 import java.util.Timer;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +10,11 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.naming.NamingException;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.conn.HttpHostConnectException;
 import org.ovirt.engine.sdk.Api;
+import org.ovirt.engine.sdk.exceptions.ServerException;
+import org.ovirt.engine.sdk.exceptions.UnsecuredConnectionAttemptError;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.dao.DbFacade;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.utils.ConfigProvider;
 import org.slf4j.Logger;
@@ -21,14 +27,16 @@ public class Backend{
     private static Api api = null;
 
     @PostConstruct
-    public void init() {
+    public void init()
+            throws ClientProtocolException, ServerException, UnsecuredConnectionAttemptError, IOException, InterruptedException {
         try {
             DbFacade.locateDataSource();
         } catch (NamingException e) {
             log.error("Error locating datasource.");
         }
-        api = getApi();
+
         int interval = Integer.parseInt(ConfigProvider.getConfig().getProperty(ConfigProvider.QUERY_INTERVAL_M));
+        api = getApi(interval);
         Timer timer = new Timer();
         timer.schedule(new FindJobs(), 0, interval);
         timer.schedule(new ExecuteSnapshot(api), 1000, interval);
@@ -46,15 +54,18 @@ public class Backend{
 		}
     }
 
-    public static Api getApi() {
+    public static Api getApi(int interval)
+            throws ClientProtocolException, ServerException, UnsecuredConnectionAttemptError, IOException, InterruptedException{
         try {
             return new Api(
                     ConfigProvider.getConfig().getProperty(ConfigProvider.SDK_BASE_URL),
                     ConfigProvider.getConfig().getProperty(ConfigProvider.SDK_USER),
                     ConfigProvider.getConfig().getProperty(ConfigProvider.SDK_PASSWORD),
                     null, 443, 10, true, true, false, false);
-        } catch (Exception e) {
-            throw new RuntimeException("api object init failed!");
+        } catch (HttpHostConnectException|NoRouteToHostException|ServerException e) {
+            log.error("Refused while getting the api connection, retrying in " + interval/1000 + " seconds...");
+            Thread.sleep(interval);
+            return getApi(interval + 10000);
         }
     }
 
