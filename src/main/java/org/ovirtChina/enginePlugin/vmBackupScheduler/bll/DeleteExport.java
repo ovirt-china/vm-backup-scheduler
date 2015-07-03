111769ac839e940d919e03d031affb6d4a22b51a
walteryang <walteryang47@gmail.com>
2015-07-03 10:20:54 +0800

    orrect the target export domain in DeleteExport logic
    Signed-off-by: walteryang <walteryang47@gmail.com>

--- src/main/java/org/ovirtChina/enginePlugin/vmBackupScheduler/bll/DeleteExport.java
+++ src/main/java/org/ovirtChina/enginePlugin/vmBackupScheduler/bll/DeleteExport.java
@@ -6,9 +6,11 @@ import org.apache.http.client.ClientProtocolException;
 import org.ovirt.engine.sdk.Api;
 import org.ovirt.engine.sdk.decorators.StorageDomain;
 import org.ovirt.engine.sdk.decorators.StorageDomainVM;
+import org.ovirt.engine.sdk.decorators.VM;
 import org.ovirt.engine.sdk.exceptions.ServerException;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.common.EngineEventSeverity;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.common.Task;
+import org.ovirtChina.enginePlugin.vmBackupScheduler.common.TaskStatus;
 import org.ovirtChina.enginePlugin.vmBackupScheduler.common.TaskType;
 
 public class DeleteExport extends DeleteOldBackupSDKTask {
@@ -19,7 +21,8 @@ public class DeleteExport extends DeleteOldBackupSDKTask {
 
     @Override
     protected void deleteTask(Task task) throws ClientProtocolException, ServerException, IOException, InterruptedException {
-        StorageDomain isoDoaminToExport = getIsoDomainToExport();
+        VM originalVm = api.getVMs().get(task.getVmID());
+        StorageDomain isoDoaminToExport = getIsoDomainToExport(originalVm);
         StorageDomainVM vm = null;
         if (isoDoaminToExport != null) {
             String message = null;
@@ -31,6 +34,12 @@ public class DeleteExport extends DeleteOldBackupSDKTask {
                 deleteTaskRecord(EngineEventSeverity.normal, message, task);
             }
             deleteTaskRecord(EngineEventSeverity.normal, message, task);
+        } else {
+            String message = "Failed to delete the export backup of vm: " + originalVm.getName() + ", aborting export backup.";
+            log.error(message);
+            addEngineEvent(EngineEventSeverity.error, message);
+            setTaskStatus(task, TaskStatus.FAILED);
+            return;
         }
     }
 
